8a6949a5059657516dbf48921ec0dd6ddebd2d19
witoAPCS <143124114+witoAPCS@users.noreply.github.com>
2024-01-23 10:58:16 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,14 +3,22 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
+                 for(int i = 1; i < e.length(); i++){
+		   int balls = e.parse(i+10); 
+                   if(isPrime(balls) == true) System.out.println(balls
+ 		 }
+		
 		System.out.println("Not the right string, tests will fail!");
 	}
 	
 
 	//Finish this function
-	public boolean isPrime(){
-		for(int i = 0; i < Math.sqrt(
-	}
-
+public boolean isPrime(double num){
+ if(num < 2) return false; 
+ for(int i = 2; i<=Math.sqrt(num); i++){
+   if(num%i ==0) return false; 
+ }
+ return true; 
+}
 }
 
